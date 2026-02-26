a7060289244c761a1b084bc6eeae68cd514ef315
Ignacio Riboldi <riboldi.ignacio@gmail.com>
2026-02-26 19:48:46 +0000

    Task 4 new updates + readme

--- MyWork/assigment04-github.py
+++ MyWork/assigment04-github.py
@@ -1,39 +1,27 @@
-from github import Github, UnknownObjectException
+from github import Github
 from config import config as cfg
 
-# Configuration
 REPO_NAME = "IgnacioRiboldi/WSA"
 FILE_PATH = "MyWork/test.txt"
 OLD_TEXT = "Andrew"
 NEW_TEXT = "Ignacio"
 COMMIT_MESSAGE = "Replace Andrew with Ignacio"
 
-# Authenticate with GitHub
 g = Github(cfg["token"])
-
-# Get the repository
 repo = g.get_repo(REPO_NAME)
+print("Repo permissions:", repo.permissions)
 
-try:
-    # Try to get the file
-    file = repo.get_contents(FILE_PATH, ref="main")
-    content = file.decoded_content.decode("utf-8")
-    updated_content = content.replace(OLD_TEXT, NEW_TEXT)
+file = repo.get_contents(FILE_PATH, ref=repo.default_branch)
+content = file.decoded_content.decode("utf-8")
 
-    # Update the file
-    repo.update_file(
-        path=FILE_PATH,
-        message=COMMIT_MESSAGE,
-        content=updated_content,
-        sha=file.sha
-    )
-except UnknownObjectException:
-    # File does not exist → create it
-    updated_content = NEW_TEXT
-    repo.create_file(
-        path=FILE_PATH,
-        message=COMMIT_MESSAGE,
-        content=updated_content
-    )
+updated_content = content.replace(OLD_TEXT, NEW_TEXT)
+
+repo.update_file(
+    path=file.path,
+    message=COMMIT_MESSAGE,
+    content=updated_content,
+    sha=file.sha,
+    branch=repo.default_branch
+)
 
 print("File updated and pushed on GitHub.")
--- README.md
+++ README.md
@@ -4,13 +4,34 @@ Programming for Data Analytics - ATU - Project
 Assigment 2: Card Draw
 
 URL: https://deckofcardsapi.com/
+
 Document Model API: https://docs.python.org/3/library/xml.dom.html
+
 Import request: https://www.w3schools.com/Python/module_requests.asp
+
 Collections -> Counter : https://docs.python.org/3/library/collections.html#collections.Counter
 
 Assigment 3 import JSON
 
 Data Source: https://www.cso.ie/en/statistics/governmentaccounts/governmentfinancestatisticsa/
+
 Import request: https://campus.datacamp.com/courses/intermediate-importing-data-in-python/interacting-with-apis-to-import-data-from-the-web-2?ex=8
+
 With open: https://www.w3schools.com/python/python_file_open.asp
+
 f.write: https://www.w3schools.com/python/python_file_write.asp
+
+Assigment 4 Git Hub
+
+Some git hub Explanations and codes:
+https://pygithub.readthedocs.io/en/latest/introduction.html
+
+Repository References:
+https://pygithub.readthedocs.io/en/v1.57/github_objects/Repository.html?utm_source=chatgpt.com
+
+Some more sources:
+https://docs.github.com/en/rest/repos?apiVersion=2022-11-28
+
+Extra example:
+https://github.com/PyGithub/PyGithub?utm_source=chatgpt.com
+
